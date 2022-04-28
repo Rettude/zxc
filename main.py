fd2f433de02a4a1dfe3e73c80756629b6885a499
914 <re2dddd@gmail.com>
2022-04-28 10:44:20 +0500

    sad2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def print_hi(name):
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, name')
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+print('asdas')
